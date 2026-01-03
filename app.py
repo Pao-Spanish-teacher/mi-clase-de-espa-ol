a03c743f3197cd9faf865b2f59555e79e415f96f
Pao-Spanish-teacher <pao.mzh16@gmail.com>
2026-01-02 23:34:05 -0500

    Update video link and email display in app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ DATOS_TEMAS = {
     "1. Saludos y Despedidas": {
         "video": "https://www.youtube.com/watch?v=hll10VBLFoQ",
         "video2": "https://www.youtube.com/watch?v=84FNM-Ni-6U",
-        "video3": "https://www.youtube.com/shorts/4txmiiR10wM", # Nuevo video 3
+        "video3": "https://www.youtube.com/watch?v=4txmiiR10wM", # Nuevo video 3
         "pdf": "minilibro Saludos.pdf",
         "frases": ["Buenos días", "¿Cómo estás?", "Mucho gusto", "Hasta mañana"]
     },
